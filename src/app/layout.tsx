391a6e247ca66511f5b4a475cdcd45307f825d7e
priyanka24081998 <priyankagoti.istosyinfotech@gmail.com>
2025-04-08 13:32:30 +0530

    scroll added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <Header />
         <Nav />
